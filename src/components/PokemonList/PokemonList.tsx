f36120f2ac0dd89e64a3aaa6831777cba3654459
netoADS <jc.neto1999@gmail.com>
2024-04-25 22:51:25 -0300

    250420242244

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
 import PokemonServices from "../../services/PokemonServices";
-import {PokemonListInterface} from "../../types/AppInterface"
+import {
+  PokemonItemListInterface,
+  PokemonListInterface,
+} from "../../types/AppInterface";
 
 const PokemonList: React.FC = () => {
   const [pagina, setPagina] = useState<number>(1);
   const [itensPorPagina, setItensPorPagina] = useState<number>(20);
+  const [pokemonItens, setPokemonItens] = useState<PokemonItemListInterface[]>(
+    []
+  );
 
   const handleListarDados = async () => {
-    const responseData: PokemonListInterface = await PokemonServices.listarPokemons({
-      itensPorPagina: itensPorPagina,
-      pagina: pagina,
-    });
+    const responseData: PokemonListInterface =
+      await PokemonServices.listarPokemons({
+        itensPorPagina: itensPorPagina,
+        pagina: pagina,
+      });
+    if (responseData.results.length > 0) {
+      setPokemonItens(responseData.results);
+    }
   };
 
   useEffect(() => {
